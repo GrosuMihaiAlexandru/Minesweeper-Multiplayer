b17cb0709c4028701405a68eb01b7b0916e43ec0
Zeighedich <razvan.bertici@yahoo.com>
2020-08-19 15:10:31 +0300

    foundation

--- Minesweeper multi/Assets/Scripts/GameBoard.cs
+++ Minesweeper multi/Assets/Scripts/GameBoard.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using UnityEngine;
 
@@ -12,30 +13,23 @@ public class GameBoard : MonoBehaviour
     [SerializeField]
     private int mineCount;
     [SerializeField]
-    private List<PanelContainer> panels;
+    private Panel panelPrefab;
 
-    [SerializeField]
-    private PanelContainer panelPrefab;
+    private List<Panel> panels = new List<Panel>();
 
-    private bool isFirstMove;
+    private bool isFirstMove = true;
 
-    // Start is called before the first frame update
     void Start()
     {
-        isFirstMove = true;
-
-        panels = new List<PanelContainer>();
-
         int id = 0;
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                PanelContainer panelContainer = Instantiate(panelPrefab, new Vector2(j, i), Quaternion.identity);
-                panelContainer.Init(id, j, i);
-                panelContainer.transform.parent = transform;
-                panelContainer.OnPanelPressed += OnPanelPress;
-                panels.Add(panelContainer);
+                Panel panel = Instantiate(panelPrefab, new Vector3(j, 0, i), Quaternion.identity, transform);
+                panel.Init(id, j, i);
+                panel.OnPanelPressed += OnPanelPress;
+                panels.Add(panel);
                 id++;
             }
         }
@@ -46,55 +40,101 @@ public class GameBoard : MonoBehaviour
         if (isFirstMove)
         {
             FirstMove(x, y);
+        } else
+        {
+            
         }
     }
 
     public void FirstMove(int x, int y)
     {
         float depth = 0.125f * width;
-        List<PanelContainer> neighbors = GetNeighbors(x, y, (int)depth);
+        List<Panel> neighbors = GetNeighbors(x, y, (int)depth);
         neighbors.Add(GetPanel(x, y));
 
-        IEnumerable<PanelContainer> mineList = panels.Except(neighbors).OrderBy(user => Random.Range(0, 100000));
-        var mineSlots = mineList.Take(mineCount).ToList().Select(z => new { z.panel.X, z.panel.Y });
+        IEnumerable<Panel> mineList = panels.Except(neighbors).OrderBy(user => Random.Range(0, 100000));
+        var mineSlots = mineList.Take(mineCount).ToList().Select(z => new { z.X, z.Y });
 
         // Place the mines
         foreach (var mineCoord in mineSlots)
         {
-            panels.Single(panel => panel.panel.X == mineCoord.X && panel.panel.Y == mineCoord.Y).panel.IsMine = true;
+            panels.Single(panel => panel.X == mineCoord.X && panel.Y == mineCoord.Y).IsMine = true;
         }
 
         // Set adjacent mines for every panel that is not a mine
-        foreach (PanelContainer openPanel in panels.Where(panel => !panel.panel.IsMine))
+        foreach (Panel openPanel in panels.Where(panel => !panel.IsMine))
         {
-            List<PanelContainer> nearbyPanels = GetNeighbors(openPanel.panel.X, openPanel.panel.Y);
-            openPanel.panel.AdjacentMines = nearbyPanels.Count(z => z.panel.IsMine);
+            List<Panel> nearbyPanels = GetNeighbors(openPanel.X, openPanel.Y);
+            openPanel.AdjacentMines = nearbyPanels.Count(z => z.IsMine);
         }
     }    
 
-    public PanelContainer GetPanel(int x, int y)
+    public Panel GetPanel(int x, int y)
     {
-        return panels.Where(panel => panel.panel.X == x && panel.panel.Y == y).ToList()[0];
+        return panels.Where(panel => panel.X == x && panel.Y == y).ToList()[0];
     }
 
-    public List<PanelContainer> GetNeighbors(int x, int y)
+    public List<Panel> GetNeighbors(int x, int y)
     {
         return GetNeighbors(x, y, 1);
     }
 
-    public List<PanelContainer> GetNeighbors(int x, int y, int depth)
+    public List<Panel> GetNeighbors(int x, int y, int depth)
     {
-        var nearbyPanels = panels.Where(panel => panel.panel.X >= (x - depth) && panel.panel.X <= (x + depth) && 
-                                        panel.panel.Y >= (y - depth) && panel.panel.Y <= (y + depth));
-        var currentPanel = panels.Where(panel => panel.panel.X == x && panel.panel.Y == y);
+        IEnumerable<Panel> nearbyPanels = panels.Where(panel => panel.X >= (x - depth) && panel.X <= (x + depth) && 
+                                        panel.Y >= (y - depth) && panel.Y <= (y + depth));
+        IEnumerable<Panel> currentPanel = panels.Where(panel => panel.X == x && panel.Y == y);
 
         return nearbyPanels.Except(currentPanel).ToList();
     }
 
     public void RevealPanel(int x, int y)
     {
-        PanelContainer selectedPanel = panels.First(panel => panel.panel.X == x && panel.panel.Y == y);
-        selectedPanel.panel.IsRevealed = true;
-        selectedPanel.panel.IsFlagged = false;
+        Panel selectedPanel = panels.First(panel => panel.X == x && panel.Y == y);
+        selectedPanel.IsRevealed = true;
+        selectedPanel.IsFlagged = false;
+
+        if (!selectedPanel.IsMine && selectedPanel.AdjacentMines == 0)
+        {
+            RevealZeros(x, y);
+
+        }
+
+        if (!selectedPanel.IsMine)
+        {
+            CompletionCheck();
+        }
+    }
+
+    public void RevealZeros(int x, int y)
+    {
+        IEnumerable<Panel> neighborPanels = GetNeighbors(x, y).Where(panel => !panel.IsRevealed);
+        foreach (var neighbor in neighborPanels)
+        {
+            neighbor.IsRevealed = true;
+            if (neighbor.AdjacentMines == 0)
+            {
+                RevealZeros(neighbor.X, neighbor.Y);
+            }
+        }
+    }
+
+    private void CompletionCheck()
+    {
+        IEnumerable<int> hiddenPanels = panels.Where(x => !x.IsRevealed).Select(x => x.ID);
+        IEnumerable<int> minePanels = panels.Where(x => x.IsMine).Select(x => x.ID);
+        if (!hiddenPanels.Except(minePanels).Any())
+        {
+        
+        }
+    }
+
+    public void FlagPanel(int x, int y)
+    {
+        Panel panel = panels.Where(z => z.X == x && z.Y == y).First();
+        if (!panel.IsRevealed)
+        {
+            panel.IsFlagged = true;
+        }
     }
 }
--- Minesweeper multi/Assets/Scripts/Panel.cs
+++ Minesweeper multi/Assets/Scripts/Panel.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
-public class Panel
+public class Panel : MonoBehaviour
 {
     public int ID { get; set; }
     public int X { get; set; }
@@ -11,11 +13,22 @@ public class Panel
     public int AdjacentMines { get; set; }
     public bool IsRevealed { get; set; }
     public bool IsFlagged { get; set; }
-    
-    public Panel(int id, int x, int y)
+
+    public void Init(int id, int x, int y)
     {
         this.ID = id;
         this.X = x;
         this.Y = y;
+    }
+
+    public event Action<int, int> OnPanelPressed;
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnPanelPressed(X, Y);
+            Debug.Log (X + " " + Y);
+        }
     }
 }
